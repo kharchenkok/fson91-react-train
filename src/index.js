03145ba1cd10696aeacdb84b00c61f533891e6a7
kharchenkok <k.v.kharchenko@ukr.net>
2023-11-20 23:14:51 +0200

    player added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
 import './styles/base.scss';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './components/GlobalStyle';
+
+const theme = {};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
